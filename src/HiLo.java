6633b49f6a12eecaef9464485c3933b16fbf015f
aswack <austin.swack@gmail.com>
2021-11-02 18:31:03 -0500

    added functionality to play game multiple times

--- src/HiLo.java
+++ src/HiLo.java
@@ -5,26 +5,30 @@ public class HiLo {
     public static void main(String[] args){
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
-        boolean playing = true;
+        boolean playing;
 
         do{
-            int tries = 1;
+            int tries = 0;
+            int theGuess = 0;
+            String playAgain;
             int theNumber = rand.nextInt(0,101);
-            System.out.println("Guess a number between 1 and 100:");
-            int theGuess = scan.nextInt();
 
             while(theGuess!=theNumber){
-                if(theGuess<theNumber) System.out.println(theGuess+" is too low. Try again");
-                else if(theGuess>theNumber) System.out.println(theGuess+" is too high. Try again");
-
                 System.out.println("Guess a number between 1 and 100:");
                 theGuess = scan.nextInt();
+                scan.nextLine();
+
+                if(theGuess<theNumber) System.out.println(theGuess+" is too low. Try again");
+                else if(theGuess>theNumber) System.out.println(theGuess+" is too high. Try again");
+                else System.out.println(theGuess+" is correct! You win!");
                 tries++;
             }
-            System.out.println(theGuess+" is correct! You win!");
-            System.out.println("It only took you "+tries+" tries! Good work!");
-            playing = false;
+            System.out.println("It only took you "+tries+" tries! Good work!\n");
+            System.out.println("Do you want to play again? (y/n)");
 
-        }while(playing == true);
+            playAgain = scan.nextLine();
+            playing = (playAgain.contains("y"));
+
+        }while(playing);
     }
 }
