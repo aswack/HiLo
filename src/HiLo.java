64f99af360c5c6f11c61aa6c36b332fc72474318
aswack <austin.swack@gmail.com>
2021-11-02 18:57:54 -0500

    increased number range, improved resource management

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,7 +1,5 @@
 import java.util.*;
-
 public class HiLo {
-
     public static void main(String[] args){
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
@@ -11,12 +9,11 @@ public class HiLo {
             int tries = 0;
             int theGuess = 0;
             String playAgain;
-            int theNumber = rand.nextInt(0,101);
+            int theNumber = rand.nextInt(-100,101);
 
             while(theGuess!=theNumber){
-                System.out.println("Guess a number between 1 and 100:");
+                System.out.println("Guess a number between -100 and 100:");
                 theGuess = scan.nextInt();
-                scan.nextLine();
 
                 if(theGuess<theNumber) System.out.println(theGuess+" is too low. Try again");
                 else if(theGuess>theNumber) System.out.println(theGuess+" is too high. Try again");
@@ -25,10 +22,11 @@ public class HiLo {
             }
             System.out.println("It only took you "+tries+" tries! Good work!\n");
             System.out.println("Do you want to play again? (y/n)");
-
-            playAgain = scan.nextLine();
+            playAgain = scan.next().toLowerCase();
             playing = (playAgain.contains("y"));
 
         }while(playing);
+        System.out.println("Thank you for playing, Goodbye!");
+        scan.close();
     }
 }
